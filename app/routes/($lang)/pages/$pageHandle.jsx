0735da26179f95302caa7bea69f8e262756c4189
Rene <renebitter@gmail.com>
2023-02-16 16:56:19 -0300

    Initial commit

---
+++ app/routes/($lang)/pages/$pageHandle.jsx
@@ -0,0 +1,67 @@
+import {json} from '@shopify/remix-oxygen';
+import {useLoaderData} from '@remix-run/react';
+import invariant from 'tiny-invariant';
+import {PageHeader} from '~/components';
+
+const seo = ({data}) => ({
+  title: data?.page?.seo?.title,
+  description: data?.page?.seo?.description,
+});
+
+export const handle = {
+  seo,
+};
+
+export async function loader({request, params, context}) {
+  invariant(params.pageHandle, 'Missing page handle');
+
+  const {page} = await context.storefront.query(PAGE_QUERY, {
+    variables: {
+      handle: params.pageHandle,
+      language: context.storefront.i18n.language,
+    },
+  });
+
+  if (!page) {
+    throw new Response(null, {status: 404});
+  }
+
+  return json(
+    {page},
+    {
+      headers: {
+        // TODO cacheLong()
+      },
+    },
+  );
+}
+
+export default function Page() {
+  const {page} = useLoaderData();
+
+  return (
+    <>
+      <PageHeader heading={page.title}>
+        <div
+          dangerouslySetInnerHTML={{__html: page.body}}
+          className="prose dark:prose-invert"
+        />
+      </PageHeader>
+    </>
+  );
+}
+
+const PAGE_QUERY = `#graphql
+  query PageDetails($language: LanguageCode, $handle: String!)
+  @inContext(language: $language) {
+    page(handle: $handle) {
+      id
+      title
+      body
+      seo {
+        description
+        title
+      }
+    }
+  }
+`;
